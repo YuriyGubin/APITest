66442282a6991ae78f486da48b6179d8031d6330
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-23 14:12:02 +0500

    Add method for alert

--- APITest/MainViewController.swift
+++ APITest/MainViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// MARK: - Enums
 enum Link: String {
     case demoURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY"
 }
@@ -34,13 +35,21 @@ enum Alert {
     }
 }
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+// MARK: - Private Methods
+    private func showAlert(withStatus status: Alert) {
+        let alert = UIAlertController(title: status.title, message: status.message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        DispatchQueue.main.async { [unowned self] in
+            present(alert, animated: true)
+        }
+    }
 }
 
